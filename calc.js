5ac95f9d812d37d33b994c9274bea317f2fb5e6b
Franciszek Szczepan Wawrzak <frank@wawrzak.com>
2011-06-20 21:59:31 +0200

    first steps

--- calc.js
+++ calc.js
@@ -1,136 +1,10 @@
 
-
-function print_r(theObj){
-	var ret="";
-	if(theObj.constructor == Array ||
-			theObj.constructor == Object){
-		ret+="<ul>";
-		for(var p in theObj){
-			if(theObj[p]!=null &&
-					(theObj[p].constructor == Array||
-					 theObj[p].constructor == Object)){
-				ret+="<li>["+p+"] => "+typeof(theObj)+"</li>";
-				ret+="<ul>";
-				ret+=print_r(theObj[p]);
-				ret+="</ul>";
-			} else {
-				ret+="<li>["+p+"] => "+theObj[p]+"</li>";
-			}
-		}
-		ret+="</ul>";
-	}
-	return ret;
-}
-
 function xmlquote(s) {
 	if(s)
 		return String(s).replace('&','&amp;').replace('<','&lt;').replace('>','&gt;').replace('\'','&apos;').replace('"','&quot;');
 	else
 		return '';
 }
-
-
-//##########################
-// http://stackoverflow.com/questions/2790001/fixing-javascript-array-functions-in-internet-explorer-indexof-foreach-etc
-
-// Add ECMA262-5 method binding if not supported natively
-//
-if (!('bind' in Function.prototype)) {
-    Function.prototype.bind= function(owner) {
-        var that= this;
-        if (arguments.length<=1) {
-            return function() {
-                return that.apply(owner, arguments);
-            };
-        } else {
-            var args= Array.prototype.slice.call(arguments, 1);
-            return function() {
-                return that.apply(owner, arguments.length===0? args : args.concat(Array.prototype.slice.call(arguments)));
-            };
-        }
-    };
-}
-
-// Add ECMA262-5 string trim if not supported natively
-//
-if (!('trim' in String.prototype)) {
-    String.prototype.trim= function() {
-        return this.replace(/^\s+/, '').replace(/\s+$/, '');
-    };
-}
-
-// Add ECMA262-5 Array methods if not supported natively
-//
-if (!('indexOf' in Array.prototype)) {
-    Array.prototype.indexOf= function(find, i /*opt*/) {
-        if (i===undefined) i= 0;
-        if (i<0) i+= this.length;
-        if (i<0) i= 0;
-        for (var n= this.length; i<n; i++)
-            if (i in this && this[i]===find)
-                return i;
-        return -1;
-    };
-}
-if (!('lastIndexOf' in Array.prototype)) {
-    Array.prototype.lastIndexOf= function(find, i /*opt*/) {
-        if (i===undefined) i= this.length-1;
-        if (i<0) i+= this.length;
-        if (i>this.length-1) i= this.length-1;
-        for (i++; i-->0;) /* i++ because from-argument is sadly inclusive */
-            if (i in this && this[i]===find)
-                return i;
-        return -1;
-    };
-}
-if (!('forEach' in Array.prototype)) {
-    Array.prototype.forEach= function(action, that /*opt*/) {
-        for (var i= 0, n= this.length; i<n; i++)
-            if (i in this)
-                action.call(that, this[i], i, this);
-    };
-}
-if (!('map' in Array.prototype)) {
-    Array.prototype.map= function(mapper, that /*opt*/) {
-        var other= new Array(this.length);
-        for (var i= 0, n= this.length; i<n; i++)
-            if (i in this)
-                other[i]= mapper.call(that, this[i], i, this);
-        return other;
-    };
-}
-if (!('filter' in Array.prototype)) {
-    Array.prototype.filter= function(filter, that /*opt*/) {
-        var other= [], v;
-        for (var i=0, n= this.length; i<n; i++)
-            if (i in this && filter.call(that, v= this[i], i, this))
-                other.push(v);
-        return other;
-    };
-}
-if (!('every' in Array.prototype)) {
-    Array.prototype.every= function(tester, that /*opt*/) {
-        for (var i= 0, n= this.length; i<n; i++)
-            if (i in this && !tester.call(that, this[i], i, this))
-                return false;
-        return true;
-    };
-}
-if (!('some' in Array.prototype)) {
-    Array.prototype.some= function(tester, that /*opt*/) {
-        for (var i= 0, n= this.length; i<n; i++)
-            if (i in this && tester.call(that, this[i], i, this))
-                return true;
-        return false;
-    };
-}
-
-//#############################################################
-
-
-
-
-
 
 
 //unique radio id
@@ -1111,6 +985,113 @@ group:false,
 
 		return x;
 	}
+
+
+
+function jsArmyCalc( selector, templateurl ){
+	
+	calc = {};
+	
+	calc.canvas = $(selector);
+	calc.canvas.attr('style','');
+	
+	calc.canvas.html('loading calc...');
+
+	// calc template is loaded with AJAX	
+	//TODO maybe it would be better to embedd calc html here?	
+	var template = $.ajax({
+				url: templateurl,
+				async: false
+				
+				}).responseText;
+		
+	calc.canvas.html(template);
+	calc.statusElem = calc.canvas.find('#acStatus');
+
+	calc.canvas.find('#acMaximize').click(function(){calc.setFullscreen(true)});
+	calc.canvas.find('#acMinimize').click(function(){calc.setFullscreen(false)});
+
+
+	//this is a 
+	calc.army = {};
+	calc.units = [];
+	calc.availableUnits = {};
+	
+ 	
+	calc.setStatus = function( text ){
+		this.statusElem.text(text);
+	}
+
+
+	calc.setStatus('no TWR file loaded');			
+
+	calc.setError = function( text ){
+		this.statusElem.html('<b style="color:red">'+text+'</b>');
+	}
+	
+	xhrError = function( jqxhr, text, error ){
+		this.calc.setError( this.url+' - '+text + ' - ' + error );
+	}
+
+	calc.loadInfoXml = function( xml ){
+		
+		ruleset = {};
+		ruleset.version = $(xml).find('ruleset').attr('version');
+		
+		ruleset.uid = $(xml).find('uid').text();
+		ruleset.revision = $(xml).find('revision').text();
+		ruleset.name = $(xml).find('name').text();
+		ruleset.mediapath = $(xml).find('files').find('media').text();
+		
+		$(xml).find('files').find('units').find('file').each(function(){
+		
+			$.ajax({
+				calc: calc,
+				url: calc.twrurl + $(this).find('path').text(),
+				success: calc.calcLoadUnitsXml,
+				error: xhrError,
+				dataType: 'xml'
+			});
+		});
+		
+
+		this.ruleset = ruleset;
+	}
+
+	calc.calcLoadUnitsXml = function( xml ){
+		console.log(xml);
+	}
+	
+	calc.loadTWR = function( url ){
+		
+		this.twrurl = url;
+		this.units = [];
+		this.availableUnits = [];
+		
+		this.setStatus( 'loading '+url );
+	
+		$.ajax({
+			calc: this,
+			url: url + 'info.xml',
+			success: this.loadInfoXml,
+			error: xhrError,
+			dataType: 'xml'
+		});
+
+	};
+
+	calc.setFullscreen = function( fs ){
+		
+		$('body').toggleClass('acFullscreen',fs);
+		this.canvas.toggleClass('acFullscreen',fs);
+		this.canvas.find('#acMaximize').toggle( !fs );
+		this.canvas.find('#acMinimize').toggle( fs );
+
+	};
+	
+	return calc;
+}
+
 
 //init function
 function initCalculator( canvasid , options ){
