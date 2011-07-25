a49281fef729e5fb40fd1b61e156f553bef30954
Franciszek Szczepan Wawrzak <frank@wawrzak.com>
2011-07-25 19:38:41 +0200

    Cleanup. removed old calc sources. moved sources to src dir. added css minification.

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-FILES="helpers.js class.ruleset.js class.instance.js class.armycalc.js"
+# This simple script merges all files in src folder into armycalc.js and armycalc.css.
+# then it uses yui-compressor to build minified versions
+
+FILES="src/ie.calc.js src/helpers.js src/class.ruleset.js src/class.instance.js src/class.armycalc.js"
+CSS_FILES="src/calc.css"
 
 cat $FILES > armycalc.js
+cat $CSS_FILES > armycalc.css
 
 yui-compressor armycalc.js -o armycalc.min.js
+yui-compressor armycalc.css -o armycalc.min.css
 
-#rm armycalc.js
 
