ae7b9f1d373d5068ece7a205a7fde021e427253d
Franciszek Szczepan Wawrzak <frank@wawrzak.com>
2012-01-28 16:16:31 +0100

    instance constructor

--- src/instance.js
+++ src/instance.js
@@ -1,12 +1,20 @@
 (function(ArmyCalc){
 	ArmyCalc.Instance = (function(){
 		
-		function Instance(template){
-			var instance = {};
-			instance.t = template;
-			instance.children = [];
-			instance.stats = {};
-			return instance;
+		function Instance(template) {
+			this.template = template;
+			this.children = [];
+			this.stats = {};
+			for (var i in template.stats) {
+			  this.stats[i] = template.stats[i];
+			}
+			this.size = template.defaultSize;
+			this.costs = {};
+			for (var i in template.costs) {
+			  this.costs[i] = template.costs[i] * this.size;
+			  this.costs[i] = template.costs[i] * this.size;
+			}
+			//TODO append all required childrens
 		}
 		
 		Instance.prototype = {
